b74fc11053986a42d95e6f795add123b60cef526
mugambi12 <silasmungiria16@gmail.com>
2024-04-10 17:41:20 +0300

    Add Money screen component in client/Components/Screens/Money/Money.js

---
+++ client/Components/Screens/Money/Money.js
@@ -0,0 +1,13 @@
+// Money.js
+import React from "react";
+import { View, Text } from "react-native";
+
+const Money = () => {
+  return (
+    <View>
+      <Text>Money Screen</Text>
+    </View>
+  );
+};
+
+export default Money;
