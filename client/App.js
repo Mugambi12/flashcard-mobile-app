7726bf55eb69ae2f423178eedb1263a97dc8652e
mugambi12 <silasmungiria16@gmail.com>
2024-04-10 17:40:59 +0300

    Refactor navigation and add new screens in App.js

--- client/App.js
+++ client/App.js
@@ -3,21 +3,24 @@ import { StatusBar } from "expo-status-bar";
 import { View, StyleSheet } from "react-native";
 import Login from "./Components/Screens/Auth/Login";
 import Register from "./Components/Screens/Auth/Register";
+import Menu from "./Components/Screens/Navigation/Menu";
+import NavBar from "./Components/Screens/Navigation/NavBar";
 import Home from "./Components/Screens/Home/Home";
+import Money from "./Components/Screens/Money/Money";
+import History from "./Components/Screens/History/History";
+import Settings from "./Components/Screens/Settings/Settings";
+import Profile from "./Components/Screens/Profile/Profile";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
+  const [currentScreen, setCurrentScreen] = useState("Home");
 
   const handleLogin = () => {
-    // Perform authentication logic here
-    // For now, simply set loggedIn to true
     setLoggedIn(true);
   };
 
   const handleLogout = () => {
-    // Perform logout logic here
-    // For now, simply set loggedIn to false
     setLoggedIn(false);
   };
 
@@ -29,14 +32,28 @@ export default function App() {
     setShowRegister(false);
   };
 
+  const navigateToScreen = (screen) => {
+    setCurrentScreen(screen);
+  };
+
   return (
     <View style={styles.body}>
+      {loggedIn && !showRegister && <Menu />}
+      {loggedIn && !showRegister && <NavBar />}
       {!loggedIn && !showRegister ? (
         <Login onLogin={handleLogin} onRegister={handleRegister} />
       ) : showRegister ? (
         <Register onLogin={handleBackToLogin} />
+      ) : currentScreen === "Home" ? (
+        <Home onLogout={handleLogout} navigateToScreen={navigateToScreen} />
+      ) : currentScreen === "Money" ? (
+        <Money />
+      ) : currentScreen === "History" ? (
+        <History />
+      ) : currentScreen === "Settings" ? (
+        <Settings />
       ) : (
-        <Home onLogout={handleLogout} />
+        <Profile />
       )}
       <StatusBar style="auto" />
     </View>
