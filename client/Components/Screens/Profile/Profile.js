8c0ea9b6e1c58068ea5f539f37f653692a1502ac
mugambi12 <silasmungiria16@gmail.com>
2024-04-10 20:56:44 +0300

    Refactor Profile component to use StyleSheet.create for styling and update title style

--- client/Components/Screens/Profile/Profile.js
+++ client/Components/Screens/Profile/Profile.js
@@ -1,13 +1,24 @@
-// Profile.js
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
 
 export default Profile;
