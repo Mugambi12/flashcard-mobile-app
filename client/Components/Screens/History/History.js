2fb66f9a6a7100f33f704d1d9b9b7d69b05902ce
mugambi12 <silasmungiria16@gmail.com>
2024-04-10 20:55:56 +0300

    Refactor History component to use StyleSheet.create for styling and update title style

--- client/Components/Screens/History/History.js
+++ client/Components/Screens/History/History.js
@@ -1,13 +1,24 @@
-// History.js
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const History = () => {
   return (
-    <View>
-      <Text>History Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>History Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
 
 export default History;
