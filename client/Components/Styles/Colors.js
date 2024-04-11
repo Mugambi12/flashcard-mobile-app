2a9bba019c9e3088662217bc3a90c95298e8ce33
mugambi12 <silasmungiria16@gmail.com>
2024-04-11 17:04:06 +0300

    Refactor Colors.js to add defaults for screenwidth, colors, padding, margin, border radius, and box shadow

--- client/Components/Styles/Colors.js
+++ client/Components/Styles/Colors.js
@@ -1,6 +1,10 @@
-// Defaul colors;
+// Defaults for screenwidth, colors, padding, margin, border radius, and box shadow
+import { Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
 
 const colors = {
+  transparent: "transparent",
   primary: "#007bff",
   secondary: "#6c757d",
   success: "#28a745",
@@ -14,4 +18,53 @@ const colors = {
   borderColor: "#ced4da",
 };
 
-export default colors;
+const padding = {
+  sm: 5,
+  md: 10,
+  lg: 15,
+  xl: 20,
+};
+
+const margin = {
+  sm: 5,
+  md: 10,
+  lg: 15,
+  xl: 20,
+  xxl: 50,
+  xxxl: 70,
+};
+
+const borderRadius = {
+  sm: 5,
+  md: 10,
+  lg: 15,
+  xl: 20,
+};
+
+const boxShadow = {
+  shadowColor: colors.black,
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 3.84,
+  elevation: 5,
+};
+
+const opacity = {
+  xs: 0.3,
+  sm: 0.5,
+  md: 0.7,
+  lg: 0.9,
+};
+
+export {
+  screenWidth,
+  colors,
+  padding,
+  margin,
+  borderRadius,
+  boxShadow,
+  opacity,
+};
